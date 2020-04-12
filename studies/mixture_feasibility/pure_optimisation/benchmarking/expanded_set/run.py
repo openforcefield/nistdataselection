abf795dc353b56f48f90fd60eb4fc0a209351847
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-04-11 20:34:26 -0600

    Add expanded benchmark inputs.

---
+++ studies/mixture_feasibility/pure_optimisation/benchmarking/expanded_set/run.py
@@ -0,0 +1,105 @@
+from evaluator import unit
+from evaluator.backends import QueueWorkerResources
+from evaluator.backends.dask import DaskLSFBackend
+from evaluator.client import ConnectionOptions, EvaluatorClient
+from evaluator.datasets import PhysicalPropertyDataSet
+from evaluator.forcefield import SmirnoffForceFieldSource
+from evaluator.server import EvaluatorServer
+from evaluator.utils import setup_timestamp_logging
+
+
+def generate_request_options(data_set, force_field_source):
+    """Decreases the target mass density of the protocol which
+    builds the gas phase coordinates as part of the enthalpy of
+    vaporization workflow. Note this will only affect the coordinate
+    building, as the gas phase simulations are performed with PBC
+    disabled.
+
+    This is a workaround for a current bug in the evaluator framework.
+    """
+
+    request_options = EvaluatorClient.default_request_options(
+        data_set, force_field_source
+    )
+
+    h_vap_schema = request_options.calculation_schemas["EnthalpyOfVaporization"][
+        "SimulationLayer"
+    ]
+    build_gas_coords_schema = next(
+        iter(
+            [
+                x
+                for x in h_vap_schema.workflow_schema.protocol_schemas
+                if x.id == "build_coordinates_gas"
+            ]
+        )
+    )
+    build_gas_coords_schema.inputs[".mass_density"] = 0.01 * unit.gram / unit.milliliter
+
+    return request_options
+
+
+def main():
+
+    setup_timestamp_logging()
+
+    # Load in the force field
+    force_field_path = "openff-1.0.0-refit.offxml"
+    force_field_source = SmirnoffForceFieldSource.from_path(force_field_path)
+
+    # Load in the test set.
+    data_set = PhysicalPropertyDataSet.from_json("full_set.json")
+
+    # Set up a server object to run the calculations using.
+    working_directory = "working_directory"
+
+    # Set up a backend to run the calculations on. This assume running
+    # on a HPC resources with the LSF queue system installed.
+    queue_resources = QueueWorkerResources(
+        number_of_threads=1,
+        number_of_gpus=1,
+        preferred_gpu_toolkit=QueueWorkerResources.GPUToolkit.CUDA,
+        per_thread_memory_limit=5 * unit.gigabyte,
+        wallclock_time_limit="05:59",
+    )
+
+    worker_script_commands = ["conda activate forcebalance", "module load cuda/10.1"]
+
+    calculation_backend = DaskLSFBackend(
+        minimum_number_of_workers=1,
+        maximum_number_of_workers=50,
+        resources_per_worker=queue_resources,
+        queue_name="gpuqueue",
+        setup_script_commands=worker_script_commands,
+        adaptive_interval="1000ms",
+    )
+
+    with calculation_backend:
+
+        server = EvaluatorServer(
+            calculation_backend=calculation_backend,
+            working_directory=working_directory,
+            port=8003,
+        )
+
+        with server:
+
+            # Generate the request options.
+            request_options = generate_request_options(data_set, force_field_source)
+
+            # Request the estimates.
+            client = EvaluatorClient(ConnectionOptions(server_port=8003))
+
+            request, _ = client.request_estimate(
+                property_set=data_set,
+                force_field_source=force_field_source,
+                options=request_options,
+            )
+
+            # Wait for the results.
+            results, _ = request.results(True, 5)
+            results.json(f"results.json")
+
+
+if __name__ == "__main__":
+    main()

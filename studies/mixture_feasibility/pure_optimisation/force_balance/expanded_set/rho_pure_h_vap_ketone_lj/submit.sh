fff4f18303f4b76648ea2db2249d8d9d56c2a1dd
owenmadin <owen.madin@colorado.edu>
2020-05-14 16:55:01 -0600

    Create separate files for keton LJ study

---
+++ studies/mixture_feasibility/pure_optimisation/force_balance/expanded_set/rho_pure_h_vap_ketone_lj/submit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Set the job name and wall time limit
+#BSUB -J forcebal
+#BSUB -W 48:00
+#
+# Set the output and error output paths.
+#BSUB -o  %J.o
+#BSUB -e  %J.e
+#
+# Set cpu options.
+#BSUB -n 1 -R "rusage[mem=4]"
+
+. ~/.bashrc
+
+# Use the right conda environment
+conda activate forcebalance
+
+# Start the estimation server.
+./run_server.py &> server_console_output.log &
+echo $! > save_pid.txt
+
+sleep 30
+
+# Run ForceBalance
+ForceBalance.py optimize.in &> force_balance.log
+
+# Kill the server
+kill -9 `cat save_pid.txt`
+rm save_pid.txt
